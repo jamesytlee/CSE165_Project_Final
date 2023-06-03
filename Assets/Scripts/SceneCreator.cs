825eb0096b8b1e0b6e3f808d366a5e4081521155
ChayPark <chp009@ucsd.edu>
2023-06-03 16:57:25 +0900

    main menu

--- Assets/Scripts/SceneCreator.cs
+++ Assets/Scripts/SceneCreator.cs
@@ -1,13 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class SceneCreator : MonoBehaviour
 {
-    public void OnCreateSceneButtonClick()
-    {
-        // Create a new scene using SceneManager
-        SceneManager.LoadScene("NewSceneName");
-    }
+    // SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
 }
