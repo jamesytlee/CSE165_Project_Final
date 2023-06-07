d0696450af23df605294b3c81967ea7c1f7c40a3
James Yuntae Lee <y2lee@ucsd.edu>
2023-06-06 17:46:48 -0700

    Adds material package and room spawner script

--- Assets/Scripts/GuardianPointDisplayer.cs
+++ Assets/Scripts/GuardianPointDisplayer.cs
@@ -1,11 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class GuardianPointDisplayer : MonoBehaviour
 {
     public GameObject sphere;
     public double RDPtolerance;
+    public float wallHeight;
+
+    public Material floorMaterial;
+    public Material wallMaterial;
 
     void Start()
     {
@@ -16,10 +21,94 @@ public class GuardianPointDisplayer : MonoBehaviour
     {
         Vector3[] guardianPoints = OVRManager.boundary.GetGeometry(OVRBoundary.BoundaryType.PlayArea);
         List<Vector3> simplifiedPoints = RDPAlgorithm.DouglasPeuckerReduction(new List<Vector3>(guardianPoints), RDPtolerance);
+        CreateMesh(simplifiedPoints, wallHeight);  // 2.0f is the default wall height
+        CreateFloor(simplifiedPoints);
 
         foreach (var position in simplifiedPoints)
         {
+            Debug.Log("Position: " + position);
             Instantiate(sphere, position, Quaternion.identity);
         }
+    }
+
+    void CreateMesh(List<Vector3> points, float wallHeight)
+    {
+        Mesh mesh = new Mesh();
+
+        Vector3[] vertices = new Vector3[points.Count * 2];
+        int[] triangles = new int[(points.Count - 1) * 6];
+
+        for (int i = 0; i < points.Count; i++)
+        {
+            // Bottom vertex
+            vertices[i * 2] = points[i];
+            // Top vertex
+            vertices[i * 2 + 1] = points[i] + Vector3.up * wallHeight;
+
+            // Skip the last point because it's the same as the first one
+            if (i < points.Count - 1)
+            {
+                // Two triangles forming a quad
+                triangles[i * 6] = i * 2;
+                triangles[i * 6 + 1] = (i + 1) * 2;
+                triangles[i * 6 + 2] = i * 2 + 1;
+
+                triangles[i * 6 + 3] = i * 2 + 1;
+                triangles[i * 6 + 4] = (i + 1) * 2;
+                triangles[i * 6 + 5] = (i + 1) * 2 + 1;
+            }
+        }
+
+        mesh.vertices = vertices;
+        mesh.triangles = triangles;
+
+        mesh.RecalculateNormals();
+
+        // Attach the created mesh to a new MeshFilter component
+        GameObject wall = new GameObject("Wall");
+        MeshFilter meshFilter = wall.AddComponent<MeshFilter>();
+        MeshRenderer meshRenderer = wall.AddComponent<MeshRenderer>();
+
+        meshFilter.mesh = mesh;
+        meshRenderer.material = wallMaterial;
+
+        wall.transform.parent = transform;
+    }
+
+    void CreateFloor(List<Vector3> points)
+    {
+        float minX = points.Min(v => v.x);
+        float minZ = points.Min(v => v.z);
+        float maxX = points.Max(v => v.x);
+        float maxZ = points.Max(v => v.z);
+
+        Vector3[] vertices = new Vector3[4]
+        {
+            new Vector3(minX, 0, minZ),
+            new Vector3(minX, 0, maxZ),
+            new Vector3(maxX, 0, minZ),
+            new Vector3(maxX, 0, maxZ)
+        };
+
+        int[] triangles = new int[6]
+        {
+            0, 2, 1,
+            2, 3, 1
+        };
+
+        Mesh mesh = new Mesh();
+        mesh.vertices = vertices;
+        mesh.triangles = triangles;
+
+        mesh.RecalculateNormals();
+
+        GameObject floor = new GameObject("Floor");
+        MeshFilter meshFilter = floor.AddComponent<MeshFilter>();
+        MeshRenderer meshRenderer = floor.AddComponent<MeshRenderer>();
+
+        meshFilter.mesh = mesh;
+        meshRenderer.material = floorMaterial;
+
+        floor.transform.parent = transform;
     }
 }
--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# CSE165_Project_Final
- 
+# TURD
+
+## Introduction
+**TURD (Turbo Universal RISC Design)** is a simple architecture designed to accurately execute 3 specific programs. Optimized for a 9-bit instruction size, it minimizes instruction types and employs a stack-based register file to save bits by only using push and pop operations. Lacking dedicated registers like load-store architectures, TURD's instructions consist of immediates, operands, and operations. Immediates are pushed onto the stack, operands either push or pop from the stack, and operations use the top two stack elements, then push the result back.
+Key features include:
+Stack-based register file: Simplifies operand and operation management within the 9-bit constraint.
+Streamlined instruction set: Categorized as immediates, operands, and operations for easier management.
+Target program optimization: Tailored to efficiently run 3 specific programs.
+RISC philosophy: Focuses on a smaller set of simple instructions for rapid execution and improved performance.
+TURD's architecture offers an efficient solution, adhering to the 9-bit instruction size limit while optimizing performance for target programs. Its stack-based register file and simplified instruction set ensure both simplicity and accuracy.
+
+## Individual Component Specification
+### Top Level
+File Name: top_level
+
+Description: The top level module serves to connect all the individual modules of our processor. In this module, we instantiate all the necessary components listed below. The inputs to the module are clock, reset and the output is the done flag. This done flag is needed for the testbench to know when our processor has completed the individual programs. Additionally, the decoding of our machine code is also done in this module. Overall, the top level serves to connect all individual components and wires to create our processor.
+
